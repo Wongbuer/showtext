8f9349f0864271c74265d4e4ce3305354497f2b4
Wongbuer <wongbuer@outlook.com>
2022-10-15 20:45:07 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,67 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="box">
+      <textarea v-model="text" class="before" placeholder="请输入多段文字"></textarea>
+      <button @click="sendText">转换</button>
+      <hr>
+      <h4>以下是结果</h4>
+      <textarea v-model="result" class="before"></textarea>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {},
+  data () {
+    return {
+      text: '',
+      result: ''
+    }
+  },
+  methods: {
+    async sendText () {
+      const sentences = this.text.split('\n')
+      console.log(sentences)
+      this.result = sentences.join('\n')
+      const { data: result } = await axios.post('http://localhost:8081/text/upload', JSON.stringify(sentences), {
+        headers: {
+          'Content-Type': 'application/json;charset=UTF-8'
+        }
+      })
+      this.result = result.join('\n')
+    }
   }
 }
 </script>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<style lang="less" scoped>
+.box {
+  margin: auto;
+  width: 500px;
+  height: 900px;
+  background-color: white;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+  textarea {
+    width: 400px;
+    height: 250px;
+    box-sizing: border-box;
+    padding: 15px;
+    border-radius: 25px;
+    resize: none;
+  }
+
+  button {
+    margin: 30px auto;
+    width: 350px;
+    height: 50px;
+    border: 0;
+    border-radius: 25px;
+    background-color: skyblue;
+  }
 }
 </style>
